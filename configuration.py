5a5e8333d08dda23f7f61cca11e3a27bfb43e927
Aidan Campbell <aidan.campbell@teamworks.com>
2025-08-08 11:38:11 -0400

    Update gt location in config

--- configuration.py
+++ configuration.py
@@ -100,7 +100,7 @@ dataset = {'SoccerNet':
                 'working_dir': 'data/FootballResults',
 
                 'images': 'images',
-                'gt': 'gt',
+                'gt': 'football_gt.json',
                 'feature_output_folder': '',
                 'sim_filtered': 'main_subject_0.4.json',
                 'gauss_filtered': 'main_subject_gauss_th=3.5_r=1.json',
